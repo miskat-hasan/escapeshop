7e3ceb7e77b38d39f9c514f896bb85be50eff5a7
Miskat Hasan <miskathasan979@gmail.com>
2026-04-14 16:53:26 +0600

    fixed/ vercel build error

--- src/Pages/SingleProductPage.jsx
+++ src/Pages/SingleProductPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ProductDetails from "../Components/ProductsPage/ProductDetails";
-import RelatedProducts from "../Components/ProductsPage/RelatedProducts.JSX";
+import RelatedProduct from "../Components/ProductsPage/RelatedProduct";
 
 const SingleProductPage = () => {
   return (
     <div>
       <ProductDetails />
-      <RelatedProducts />
+      <RelatedProduct />
     </div>
   );
 };
